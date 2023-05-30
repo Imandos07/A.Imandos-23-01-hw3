db129f8962ec3f1a3081239e8aac46c7a4277658
Imandos07 <aliserovimandos07@gmail.com>
2023-05-31 01:13:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+
+const updateUser = (user) => ({
+  type: 'UPDATE_USER',
+  payload: user,
+});
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_USER':
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload,
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  user: {
+    name: '',
+    age: 0,
+    gender: '',
+  },
+};
+
+const store = createStore(reducer, initialState);
+
+const UserForm = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    dispatch(updateUser({ [name]: value }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        name="name"
+        value={user.name}
+        onChange={handleInputChange}
+        placeholder="Имя"
+      />
+      <input
+        type="number"
+        name="age"
+        value={user.age}
+        onChange={handleInputChange}
+        placeholder="Возраст"
+      />
+      <select
+        name="gender"
+        value={user.gender}
+        onChange={handleInputChange}
+      >
+        <option value="">Выберите пол</option>
+        <option value="male">Мужской</option>
+        <option value="female">Женский</option>
+      </select>
+      <div>
+        <h2>Данные пользователя:</h2>
+        <p>Имя: {user.name}</p>
+        <p>Возраст: {user.age}</p>
+        <p>Пол: {user.gender}</p>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Provider store={store}>
+    <UserForm />
+  </Provider>
+);
 
 export default App;
